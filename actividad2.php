27d2ae559a33c9db8cd20af481c4f176a0e8716a
arantxacamil <marcela.galindo@comunidad.unam.mx>
2020-05-22 13:40:47 -0500

    FEAT: actividad del detective

---
+++ actividad2.php
@@ -0,0 +1,5 @@
+<?php
+echo "Arantxa Junco <br> ";
+echo "Considero que las dos cosas más importantes que he aprendido son a desarrollar pensamiento de programación y que html no es un lenguaje de programación. <br> ";
+echo "Lo que más me ha gustado es el módulo 0";
+?>
